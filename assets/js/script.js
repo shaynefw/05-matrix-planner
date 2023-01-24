d380a9a0ac93c3875b22b9aacade6431de1cd706
swmac <shaynefw@gmail.com>
2023-01-24 09:17:02 -0500

    added styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,5 @@ setInterval(function(){
     let dateAndTime = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
     $('#todaysDate').text(dateAndTime);
   }, 1000);
-  
+
+  $("li").addClass("d-flex justify-content-between");
